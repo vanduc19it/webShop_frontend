3963a5d77fa9df73f00bb106c4352d54bde1c53d
vanduc19it <duc19it@gmail.com>
2022-10-21 22:40:07 +0700

    handle register  login logout

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,22 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { register } from "../Redux/Actions/userActions";
 import Header from "./../components/Header";
-
-const Register = () => {
+import Message from "./../components/LoadingError/Error";
+import Loading from "./../components/LoadingError/Loading";
+const Register = ({location, history}) => {
   window.scrollTo(0, 0);
+  const [username, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
 
+  const dispatch = useDispatch();
+  const redirect = location.search ? location.search.split("=")[1] : "/";
+
+  const userRegister = useSelector((state) => state.userRegister);
+  const { error, loading, userInfo} = userRegister;
+
+  useEffect(()=> {
+    if (userInfo) {
+      history.push(redirect);
+    }
+  },[userInfo, history, redirect]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(register(username, email, password))
+  }
   return (
     <>
       <Header />
       <div className="container d-flex flex-column justify-content-center align-items-center login-center">
-        <form className="Login col-md-8 col-lg-4 col-11">
-          <input type="text" placeholder="Username" />
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
+        {error && <Message variant="alert-danger">{error}</Message>}
+        {loading && <Loading/>}
+        <form className="Login col-md-8 col-lg-4 col-11" onSubmit={submitHandler}>
+          <input type="text" placeholder="Username" value={username} onChange={(e) => setName(e.target.value)}/>
+          <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+          <input type="password" placeholder="Password" value= {password} onChange={(e) => setPassword(e.target.value)}/>
+          <input type="password" placeholder="Confirm Password" />
 
-          <button type="submit">Register</button>
+          <button type="submit" >Register</button>
           <p>
-            <Link to={"/login"}>
+            <Link to={redirect ? `/login?redirect=${redirect}` : "/login"}>
               I Have Account <strong>Login</strong>
             </Link>
           </p>
