61fe912e2fc97f71dbf13613385695063a94773e
vanduc19it <duc19it@gmail.com>
2023-01-13 01:53:29 +0700

    login gg

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import App from "./App";
 import store from "./Redux/store"
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
 
 ReactDOM.render(
     <Provider store={store}>
+        <GoogleOAuthProvider clientId="591909428129-7sqneikm8mv6m21p9kcasetg3vdhtt71.apps.googleusercontent.com"> 
         <App />
+        </GoogleOAuthProvider>
+
+       
     </Provider>
     ,document.getElementById("root")
 );
--- src/screens/Register.js
+++ src/screens/Register.js
@@ -5,11 +5,14 @@ import { register } from "../Redux/Actions/userActions";
 import Header from "./../components/Header";
 import Message from "./../components/LoadingError/Error";
 import Loading from "./../components/LoadingError/Loading";
+import { GoogleLogin } from '@react-oauth/google';
+import jwt_decode from "jwt-decode";
 const Register = ({location, history}) => {
   window.scrollTo(0, 0);
   const [username, setName] = useState("")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
 
   const dispatch = useDispatch();
   const redirect = location.search ? location.search.split("=")[1] : "/";
@@ -25,11 +28,15 @@ const Register = ({location, history}) => {
 
   const submitHandler = (e) => {
     e.preventDefault();
-    dispatch(register(username, email, password))
+    if(password == confirmPassword) {
+      dispatch(register(username, email, password))
+    }
+    
   }
   return (
     <>
       <Header />
+      
       <div className="container d-flex flex-column justify-content-center align-items-center login-center">
         {error && <Message variant="alert-danger">{error}</Message>}
         {loading && <Loading/>}
@@ -37,9 +44,34 @@ const Register = ({location, history}) => {
           <input type="text" placeholder="Username" value={username} onChange={(e) => setName(e.target.value)}/>
           <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
           <input type="password" placeholder="Password" value= {password} onChange={(e) => setPassword(e.target.value)}/>
-          <input type="password" placeholder="Confirm Password" />
+          <input type="password" placeholder="Confirm Password" value= {confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}/>
 
           <button type="submit" >Register</button>
+          <div className="col-md-12" style={{marginTop: '30px', marginLeft: '80px'}}>
+          <GoogleLogin
+        onSuccess={credentialResponse => {
+          console.log(credentialResponse);
+          var decoded = jwt_decode(credentialResponse.credential);
+          console.log(decoded);
+          if(decoded.name && decoded.email) {
+            setName(decoded.name)
+            setEmail(decoded.email)
+            setPassword(123)
+            console.log(username, email, password)
+            
+          }
+          
+          if(email !== ""  && username !== "") {
+            dispatch(register(username, email, password))
+          }
+          
+        }}
+        onError={() => {
+          console.log('Login Failed');
+        }}
+        useOneTap
+      />;
+          </div>
           <p>
             <Link to={redirect ? `/login?redirect=${redirect}` : "/login"}>
               I Have Account <strong>Login</strong>
